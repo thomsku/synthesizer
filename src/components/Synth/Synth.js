ddcf89b6b381b359d00337d4ecc31e07a0c67ac0
Thomas <thomas.skuggevik@gmail.com>
2019-06-06 17:34:39 +0200

    connected tuning, waveform and envelope coontrols to oscillators

--- src/components/Synth/Synth.js
+++ src/components/Synth/Synth.js
@@ -3,22 +3,10 @@ import AudioAnalyser from "../AudioAnalyser/AudioAnalyser";
 import Tuning from "../Tuning/Tuning";
 import Waveform from "../Waveform/Waveform";
 import Envelope from "../Envelope/Envelope";
+import {justKeyToFrequency, equalKeyToFrequency} from "./frequencies.js";
 import "./Synth.css";
+
 const EnvGen = require("fastidious-envelope-generator");
-
-// This is tuned to just intonation
-const keyToFrequency = {
-  a: 261.63,
-  s: 294.33,
-  d: 327.03,
-  f: 392.44,
-  g: 436.05,
-  h: 523.25,
-  j: 588.66,
-  k: 654.06,
-  l: 697.66,
-};
-
 
 class Synth extends React.Component {
   constructor(props) {
@@ -27,18 +15,20 @@ class Synth extends React.Component {
     this.state = {
       activeOscillators: {},
       audioData: new Uint8Array(0),
-      tuning: "just",
+      tuning: {type: "just", frequencies: justKeyToFrequency},
       waveform: "sine",
+      envelope: {attack: 0.1, decay: 0.01, sustain: 1, release: 0.1},
     };
 
     this.counter = 0;
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.handleKeyUp = this.handleKeyUp.bind(this);
-    this.tick = this.tick.bind(this);
     this.play = this.play.bind(this);
     this.stop = this.stop.bind(this);
     this.setTuning = this.setTuning.bind(this);
     this.setWaveform = this.setWaveform.bind(this);
+    this.setEnvelope = this.setEnvelope.bind(this);
+    // this.tick = this.tick.bind(this);
   }
 
   componentDidMount() {
@@ -50,7 +40,7 @@ class Synth extends React.Component {
     this.dataArray = new Uint8Array(this.analyser.frequencyBinCount);
     document.addEventListener("keydown", this.handleKeyDown);
     document.addEventListener("keyup", this.handleKeyUp);
-    this.rafId = requestAnimationFrame(this.tick);
+    // this.rafId = requestAnimationFrame(this.tick);
   }
 
   componentWillUnmount() {
@@ -59,18 +49,18 @@ class Synth extends React.Component {
     // this.source.disconnect();
   }
 
-  tick() {
-    if ((this.counter & 7) === 0) {
-      this.analyser.getByteTimeDomainData(this.dataArray);
-      this.setState({audioData: this.dataArray});
-    }
-    this.rafId = requestAnimationFrame(this.tick);
-    this.counter += 1;
-  }
+  // tick() {
+  //   if ((this.counter & 7) === 0) {
+  //     this.analyser.getByteTimeDomainData(this.dataArray);
+  //     this.setState({audioData: this.dataArray});
+  //   }
+  //   this.rafId = requestAnimationFrame(this.tick);
+  //   this.counter += 1;
+  // }
 
   handleKeyDown(event) {
     const key = event.key;
-    if (!this.state.activeOscillators[key] && keyToFrequency[key]) {
+    if (!this.state.activeOscillators[key] && this.state.tuning.frequencies[key]) {
       this.play(key);
     }
   }
@@ -84,17 +74,18 @@ class Synth extends React.Component {
 
   play(key) {
     const oscillator = this.audioContext.createOscillator();
-    oscillator.frequency.setValueAtTime(keyToFrequency[key], this.audioContext.currentTime);
+    oscillator.frequency.value = this.state.tuning.frequencies[key];
+    oscillator.type = this.state.waveform;
     const amp = this.audioContext.createGain();
     amp.gain.value = 0.2;
     amp.connect(this.analyser);
     amp.connect(this.audioContext.destination);
     const envelope = new EnvGen(this.audioContext, amp.gain);
     envelope.mode = "ADSR";
-    envelope.attackTime = 0.01;
-    envelope.decayTime = 0.01;
-    envelope.sustainLevel = 0;
-    envelope.releaseTime = 0.1;
+    envelope.attackTime = this.state.envelope.attack;
+    envelope.decayTime = this.state.envelope.decay;
+    envelope.sustainLevel = this.state.envelope.sustain;
+    envelope.releaseTime = this.state.envelope.release;
     this.setState((prevState) => ({
       activeOscillators: {
         ...prevState.activeOscillators,
@@ -108,16 +99,30 @@ class Synth extends React.Component {
 
   stop(key) {
     this.state.activeOscillators[key].envelope.gateOff(this.audioContext.currentTime);
-    // this.state.activeOscillators[key].oscillator.stop();
+    // stop oscillator after 10 seconds, mostly for performance. Does it actually affect it? Will have to research that
+    this.state.activeOscillators[key].oscillator.stop(this.audioContext.currentTime + 10);
     delete this.state.activeOscillators[key];
   }
 
   setTuning(event) {
-    this.setState({tuning: event.target.value});
+    let frequencies = justKeyToFrequency;
+    if (event.target.value === "equal") {
+      frequencies = equalKeyToFrequency;
+    }
+    this.setState({tuning: {type: event.target.value, frequencies: frequencies}});
   }
 
   setWaveform(event) {
     this.setState({waveform: event.target.value});
+  }
+
+  setEnvelope(type, value) {
+    this.setState((prevState) => ({
+      envelope: {
+        ...prevState.envelope,
+        [type]: value,
+      },
+    }));
   }
 
   render() {
@@ -126,9 +131,9 @@ class Synth extends React.Component {
         <AudioAnalyser activeOscillators={this.state.activeOscillators}/>
         <h2>Synth Parameters</h2>
         <div className="parameters">
-          <Tuning setTuning={this.setTuning} tuning={this.state.tuning} />
+          <Tuning setTuning={this.setTuning} tuning={this.state.tuning.type} />
           <Waveform setWaveform={this.setWaveform} waveform={this.state.waveform}/>
-          <Envelope setEnvelope={this.setEnvelope}/>
+          <Envelope setEnvelope={this.setEnvelope} envelope={this.state.envelope}/>
         </div>
       </div>
     );
---
+++ src/components/Synth/frequencies.js
@@ -0,0 +1,25 @@
+// This is tuned to just intonation
+export const justKeyToFrequency = {
+  a: 261.63, // C
+  s: 294.33, // D
+  d: 327.03, // E
+  f: 392.44, // G
+  g: 436.05, // A
+  h: 523.25, // C
+  j: 588.66, // D
+  k: 654.06, // E
+  l: 697.66, // F
+};
+
+// This is tuned to equal temperament
+export const equalKeyToFrequency = {
+  a: 261.63, // C
+  s: 293.66, // D
+  d: 329.63, // E
+  f: 392.00, // G
+  g: 440.00, // A
+  h: 523.25, // C
+  j: 587.32, // D
+  k: 659.26, // E
+  l: 698.46, // F
+};
